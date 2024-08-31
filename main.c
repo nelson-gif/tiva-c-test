18991ceedb536c897351df59ba80e016e763d9a4
nelson-gif <nelsonvelasquez2.nv@gmail.com>
2024-08-31 16:39:01 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/**
+ * Librerias
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/tm4c123gh6pm.h"
+
+//librerias extra
+#include "driverlib/sysctl.h" //reloj
+#include "driverlib/gpio.h"
+#include "driverlib/adc.h" //interrupciones
+#include "driverlib/debug.h"
+
+int main(void){
+
+}
